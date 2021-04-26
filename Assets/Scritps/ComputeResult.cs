ecab1069ff209171251279c37fd6e7bb912892a9
Nico <nicolas.bachleitner03@htl-salzburg.ac.at>
2021-04-26 08:43:34 +0200

    finished first scene

---
+++ Assets/Scritps/ComputeResult.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComputeResult : MonoBehaviour
+{
+    [SerializeField] Toggle toggle1;
+    [SerializeField] Toggle toggle2;
+
+    bool toggleOneTrue;
+    bool toggleTwoTrue;
+
+    public void ComputeResultButton(bool andPressed)
+    {
+        toggleOneTrue = toggle1.GetComponent<Toggle>().isOn;
+        toggleTwoTrue = toggle2.GetComponent<Toggle>().isOn;
+        if (andPressed)
+        {
+            if (toggleOneTrue && toggleTwoTrue)
+            {
+                GetComponent<Text>().text = "Wahr";
+            }
+            else
+            {
+                GetComponent<Text>().text = "Falsch";
+            }
+        }
+        else
+        {
+            if (!toggleOneTrue && !toggleTwoTrue)
+            {
+                GetComponent<Text>().text = "Falsch";
+            }
+            else
+            {
+                GetComponent<Text>().text = "Wahr";
+            }
+        }
+    }
+}
---
+++ Assets/Scritps/ToggleManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ToggleManager : MonoBehaviour
+{
+    bool isTrue = true;
+    public void SwitchedToggle()
+    {
+        if (isTrue)
+        {
+            GetComponent<Text>().text = "Falsch";
+            isTrue = false;
+        }
+        else
+        {
+            GetComponent<Text>().text = "Wahr";
+        }
+    }
+}
